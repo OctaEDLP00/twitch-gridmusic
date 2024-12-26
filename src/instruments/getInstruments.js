e464647cb29b37575a0874b1a8fc7b75f77fc984
DevMetal00 <42822581+OctaEDLP00@users.noreply.github.com>
2024-12-26 00:01:39 -0300

    feat(instruments): add jsdoc and rename function

--- src/instruments/getInstruments.js
+++ src/instruments/getInstruments.js
@@ -1,5 +1,10 @@
-export const getInstruments = () => {
-  const files = import.meta.glob("../../public/instruments/*.flac")
+/**
+ * Obtiene una lista de nombres de instrumentos a partir de archivos FLAC almacenados en un directorio específico.
+ * @param {string | undefined} [path]
+ * @returns {Array<string>} Una lista con los nombres de los instrumentos disponibles.
+ */
+export const getFiles = (path) => {
+  const files = import.meta.glob(`../../public/${path ? path : "instruments"}/*.flac`)
   const instruments = Object.keys(files).map(key => key.split("/").at(-1).replace(".flac", ""));
   return instruments;
 }
